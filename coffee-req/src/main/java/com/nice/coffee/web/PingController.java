d84eb47f9932e394c2022b6c189771cb68832428
OhadR <ohadr.developer@gmail.com>
2014-04-29 10:41:01 +0300

    set up the proj

--- coffee-req/src/main/java/com/nice/coffee/web/PingController.java
+++ coffee-req/src/main/java/com/nice/coffee/web/PingController.java
@@ -1,10 +1,25 @@
 package com.nice.coffee.web;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class PingController 
 {
-	public static void main(String[] args)
-	{
-		System.out.println("ohad ohad");
-	}
+	private static Logger log = Logger.getLogger(PingController.class);
 
+	@RequestMapping(value = "/ping")	
+	protected void ping(
+			HttpServletRequest request,
+			HttpServletResponse response) throws Exception
+	{
+		log.info( "got to ping" );
+		System.out.print( "pong" );
+		response.getWriter().println("ping response: pong");
+
+	}
 }
