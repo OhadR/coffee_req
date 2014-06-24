b34005123d3e5a9867e32d9084763672c2e0f706
OhadR <ohadr.developer@gmail.com>
2014-06-24 22:10:57 +0300

    #15 : only NICE domain is allowed

--- coffee-req/src/main/java/com/nice/coffee/backend/CustomCreateAccountEndpoint.java
+++ coffee-req/src/main/java/com/nice/coffee/backend/CustomCreateAccountEndpoint.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import com.ohadr.auth_flows.web.AuthenticationFlowsException;
+import com.ohadr.auth_flows.web.CreateAccountEndpoint;
 
 
 @Component
@@ -14,6 +15,14 @@ public class CustomCreateAccountEndpoint extends CreateAccountEndpoint
 	public void additionalValidations(String email, String password) throws AuthenticationFlowsException 
 	{
 		log.info("this is a custom message from CustomCreateAccountEndpoint");
+		
+		String[] parts = email.split("@");
+		String domain = parts[1];
+		if( ! domain.equalsIgnoreCase( "nice.com" ) )
+		{
+			log.error("domain is: " + domain);
+			throw new AuthenticationFlowsException( "only emails with NICE domain are acceptable." );
+		}
 		
 		super.additionalValidations(email, password);
 	}
